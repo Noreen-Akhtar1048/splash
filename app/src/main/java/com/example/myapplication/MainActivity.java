8fbaf51639d04e2a86ee92a04a4843589a12ccba
Noreen Akhtar <mughalnoreen688@gmail.com>
2021-09-24 18:12:05 +0500

    splash

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import java.util.Objects;
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setTitle("Login");
 
         btn_signup = findViewById(R.id.button2);
-        btn_signup.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(),signup.class)));
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,signup.class);
+                startActivity(intent);
+            }
+        });
     }
 }
